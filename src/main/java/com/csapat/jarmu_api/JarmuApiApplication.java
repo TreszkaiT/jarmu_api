fbeaee20378709db1bf40208dfc67daae608f361
TreszkaiT <treszkaitom@gmail.com>
2023-03-29 17:44:04 +0200

    Init

---
+++ src/main/java/com/csapat/jarmu_api/JarmuApiApplication.java
@@ -0,0 +1,13 @@
+package com.csapat.jarmu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarmuApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarmuApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csapat/jarmu_api/JarmuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csapat.jarmu_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarmuApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
