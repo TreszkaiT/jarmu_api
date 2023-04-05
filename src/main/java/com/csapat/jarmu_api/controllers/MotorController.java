997aaed8f836be9f92b5eda4aceb7d83e0064fe8
mankamolnar <markmanomolnar@gmail.com>
2023-04-05 09:56:27 -0600

    controller get-all motor endpoint added

--- src/main/java/com/csapat/jarmu_api/controllers/MotorController.java
+++ src/main/java/com/csapat/jarmu_api/controllers/MotorController.java
@@ -1,7 +1,20 @@
 package com.csapat.jarmu_api.controllers;
 
+import com.csapat.jarmu_api.domain.Motor;
+import com.csapat.jarmu_api.services.MotorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class MotorController {
+
+    private MotorService motorService = new MotorService();
+
+    @GetMapping("/all-motors")
+    public List<Motor> getAllMotors() {
+        return motorService.getAll();
+    }
 }
