d79a728933be2c234b861ddabcfe829b5435ac35
mankamolnar <markmanomolnar@gmail.com>
2023-04-05 09:55:03 -0600

    repository added for motor

--- src/main/java/com/csapat/jarmu_api/services/MotorService.java
+++ src/main/java/com/csapat/jarmu_api/services/MotorService.java
@@ -1,4 +1,15 @@
 package com.csapat.jarmu_api.services;
 
+import com.csapat.jarmu_api.domain.Motor;
+import com.csapat.jarmu_api.repositories.MotorRepository;
+
+import java.util.List;
+
 public class MotorService {
+
+    private MotorRepository motorRepository = new MotorRepository();
+
+    public List<Motor> getAll() {
+        return motorRepository.getAll();
+    }
 }
